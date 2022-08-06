104a6fa6f2d2d784991e46268e11caa8a5df279f
ShubhamVsCode <shubhamvscode@gmail.com>
2022-08-06 10:41:11 +0530

    Cleaned

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
 import "./App.css";
-import { getLinkPreview } from "link-preview-js";
-import { useState } from "react";
 
 function App() {
-  const [link, setLink] = useState("");
-
-  const getData = (e) => {
-    e.preventDefault();
-
-    const data = getLinkPreview(link);
-    data.then((data) => console.log(data));
-  };
-
-  return (
-    <div className="text-3xl text-center text-black-96 App">
-      Hello
-      <form action="">
-        <input type="text" onChange={(e) => setLink(e.target.value)} />
-        <button type="submit" onSubmit={getData}>
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+  return <div className="text-3xl text-center text-black-96 App"></div>;
 }
 
 export default App;
--- src/components/Link.js
+++
@@ -1,5 +0,0 @@
-const { getLinkPreview, getPreviewFromContent } = require("link-preview-js");
-// pass the link directly
-getLinkPreview("https://www.youtube.com/watch?v=MejbOFk7H6c").then((data) =>
-  console.debug(data)
-);
